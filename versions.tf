ef5ed79ca6332f742b354dc4e4d8c5e43cf3ae32
Marcelo Mendonça <mendonca2709@gmail.com>
2021-10-06 11:23:59 -0300

    Update version constraints (#1)

--- versions.tf
+++ versions.tf
@@ -19,15 +19,15 @@ terraform {
   required_providers {
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.1.0"
+      version = ">= 3.1.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = ">= 3.1.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.60"
+      version = ">= 3.60"
     }
   }
 
