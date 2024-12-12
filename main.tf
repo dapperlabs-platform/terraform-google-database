f25315ffc0f78ec77a9524f62ce8c481ffe2edd8
michaelgardner87 <michael.gardner@axiomzen.co>
2024-12-12 10:52:48 -0800

    add PAM

--- main.tf
+++ main.tf
@@ -230,3 +230,52 @@ resource "null_resource" "module_depends_on" {
     value = length(var.module_depends_on)
   }
 }
+
+# Google PAM for CloudSQL
+resource "google_privileged_access_manager_entitlement" "entitlement" {
+  for_each = var.pam_access
+
+  entitlement_id       = each.value.name
+  location             = "global"
+  max_request_duration = each.value.max_time
+  parent               = "projects/${var.project_id}"
+  requester_justification_config {
+    unstructured {}
+  }
+  eligible_users {
+    principals = each.value.requesters
+  }
+  privileged_access {
+    gcp_iam_access {
+      role_bindings {
+        role = "roles/cloudsql.client"
+        #condition_expression = "request.time < timestamp(\"2024-04-23T18:30:00.000Z\")"
+      }
+      resource      = "//cloudresourcemanager.googleapis.com/projects/${var.project_id}"
+      resource_type = "cloudresourcemanager.googleapis.com/Project"
+    }
+  }
+  # additional_notification_targets {
+  #   admin_email_recipients = [
+  #     "user@example.com",
+  #   ]
+  #   requester_email_recipients = [
+  #     each.value.requesters
+  #   ]
+  # }
+
+  dynamic "approval_workflow" {
+    for_each = each.value.auto_approve == true ? [] : [1]
+    content {
+      manual_approvals {
+        require_approver_justification = true
+        steps {
+          approvals_needed = 1
+          approvers {
+            principals = each.value.approvers
+          }
+        }
+      }
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -336,3 +336,15 @@ variable "enable_default_user" {
   type        = bool
   default     = true
 }
+
+variable "pam_access" {
+  type = map(object({
+    name         = string
+    role         = string
+    max_time     = string
+    auto_approve = bool
+    requesters   = list(string)
+    approvers    = optional(list(string)) # Required if auto_approve is true
+  }))
+  default = {}
+}
