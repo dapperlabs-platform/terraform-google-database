8b2f67b455658f9fc9ec5285185e6777068016c3
Tommy Cho <tommy.cho@dapperlabs.com>
2026-01-12 14:57:27 -0800

    dont set default

--- variables.tf
+++ variables.tf
@@ -352,5 +352,4 @@ variable "final_backup_enabled" {
 variable "final_backup_retention_days" {
   description = "The number of days to retain the final backup"
   type        = number
-  default     = null
 }
